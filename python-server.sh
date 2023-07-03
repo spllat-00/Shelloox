9f1bb7987e840a97ac918cf2c8adc5b065f050dd
Payas Kale <50944153+spllat-00@users.noreply.github.com>
2023-07-03 12:55:54 +0530

    pyserver: feat(Selectable Network Interface)

--- python-server.sh
+++ python-server.sh
@@ -3,7 +3,7 @@
 
 # Function to handle Ctrl+C
 ctrl_c(){
-	echo -e "\nExit Command Detected. Stopping the server..."
+	echo -e "\n\nExit Command Detected. Stopping the python-server..."
 	# Add any cleanup or additional actions you need here
 	exit 0
 }
@@ -21,7 +21,7 @@ directory_exists(){
 print_files(){
 	local dir="$1"
 	local indent="$2"
-	supported_extensions=("py" "c" "cpp" "php")
+	#supported_extensions=("py" "c" "cpp" "php")
 
 	# Iterate over the files and folders in the directory
 	for item in "$dir"/*; do
@@ -32,7 +32,7 @@ print_files(){
 			case "$extension" in
 				sh)
 					# Shell script - print in green color
-					echo -e "${indent}└── ${BLUE}$name${NC}"
+					echo -e "${indent}└── ${CYAN}$name${NC}"
 					;;
 				py | c | php)
 					# Files with extensions py, c, php - print in purple color
@@ -54,10 +54,11 @@ print_files(){
 }
 
 directory_printing(){
-	echo "Directory Tree:"
-	echo ""
+	echo -e "${ORANGE}[+]    Directory Tree${NC}\n"
+	echo -e "$folderName"
 	print_files "$directory" ""
-	printf "\n=========================\n\n"
+	echo
+	# printf "\n=========================\n\n"
 }
 
 common_links(){
@@ -66,31 +67,91 @@ common_links(){
 		ip_address=$backup_link
 		echo -e "${RED}tun0 doesn't exists, using eth0${NC}"
 	fi
-	echo "Showing commonly used links"
-	echo "-------------------------"
-	echo -e "${DULL_RED}http://${ip_address}:${port}/privEsc/linpeas.sh${NC}"
+	echo -e "${ORANGE}[+]    Common Links${NC}\n"
+	echo -e "http://${ip_address}:${port}/privEsc/linpeas.sh"
 	echo ""
+}
+
+# Function to display available network interfaces
+display_interfaces() {
+    echo "Available network interfaces:"
+    interfaces=($(ip -o link show | awk '/state UP/ || /state UNKNOWN/ {print $2}' | tr -d ':'))
+    indentation="    "  # Specify the desired indentation here
+    for ((i = 0; i < ${#interfaces[@]}; i++)); do
+        interface=${interfaces[i]}
+        ip_address=$(ip -o -4 addr show dev "$interface" | awk '{split($4, a, "/"); print a[1]}')
+        echo -e "${indentation}$((i + 1)). $interface\t\t$ip_address"
+    done
+}
+
+# Function to select a network interface
+select_interface() {
+    echo "Please select your preference number:"
+    display_interfaces
+    echo -n "Enter number: "
+    read -r selected_index
+    echo ""
+    if [[ $selected_index =~ ^[0-9]+$ ]]; then
+        selected_index=$((selected_index - 1))
+        if ((selected_index >= 0 && selected_index < ${#interfaces[@]})); then
+            interface=${interfaces[selected_index]}
+        else
+            echo "Error: Invalid selection."
+            exit 1
+        fi
+    else
+        echo "Error: Invalid input."
+        exit 1
+    fi
+}
+
+ip_fetcher(){
+	echo -e "${ORANGE}[+]    IP Selection${NC}"
+	# Default interface selection logic
+	if [ -z "$interface" ]; then
+		tun_interfaces=($(ip -o link show | awk '/tun[0-9]+/ && (/state UP/ || /state UNKNOWN/) {print $2}' | tr -d ':'))
+
+		if [[ ${#tun_interfaces[@]} -gt 1 || $ask_interface ]]; then
+			[ -z "$ask_interface" ] && echo "${DULL_YELLOW}Multiple TUNnels found${NC}"
+			select_interface
+		elif [[ ${#tun_interfaces[@]} -eq 1 ]]; then
+			interface=${tun_interfaces[0]}
+		else
+			interface="eth0"
+		fi
+	fi
+
+	ip_address=$(ip addr show dev "$interface" 2>/dev/null | awk '$1 == "inet" { split($2, ip, "/"); print ip[1] }')
+	if [ -z "$ip_address" ]; then
+		echo -e "No IP address found for interface $interface."
+		exit 1
+	fi
+	echo -e "Selected: $interface\t$ip_address\n"
 }
 
 start_server(){
 	# Change to the specified directory
 	# cd "$directory" || exit
-	echo "Starting HTTP server on port $port"
+	echo -e "${ORANGE}[+]    Starting server${NC}"
+	echo -e "Link: ${BLUE}http://$ip_address:$port${NC}"
 	echo "Serving files from directory: $directory"
 	printf "\n"
 	# Start the server
-	python3 -m http.server "$port" --directory "$directory"
+	python3 -m http.server "$port" --directory "$directory" --bind "$ip_address"
 }
 
 display_help() {
 	echo "Usage: $0 [OPTIONS]"
 	echo "Options:"
 	echo "  -h, --help             Shows this help menu"
+	echo "  -n, --interface        Specify the network interface"
 	echo -e "EXAMPLE:"
 	echo -e "   $0"
 }
 
 #--------------------------RUNNER CODE--------------------------
+
+interface=""
 
 # Flag handling using getopts
 while [[ $# -gt 0 ]]; do
@@ -98,6 +159,13 @@ while [[ $# -gt 0 ]]; do
 		-h|--help)
 			display_help
 			exit 0
+		;;
+		-n|--interface)
+			ask_interface=true
+			if ! [ -z "$2" ]; then
+				interface=$2
+				shift
+			fi
 		;;
 		*)
 			echo "Error: Invalid option: $1"
@@ -118,7 +186,9 @@ DULL_RED="${C}[0;31m"
 RED="${C}[1;31m"
 PURPLE="${C}[1;35m"
 DULL_YELLOW="${C}[0;33m"
-BLUE="${C}[1;32m"
+ORANGE="${C}[1;33m"
+CYAN="${C}[1;32m"
+BLUE="${C}[1;34m"
 NC="${C}[0m"
 
 # Trap the SIGINT signal (Ctrl+C)
@@ -126,10 +196,10 @@ trap ctrl_c SIGINT
 
 port=9092
 scriptFilePath=$(realpath "$0")
-directory="${scriptFilePath%/*}/PythonServer" # Change for custom directory
+folderName="PythonServer"
+directory="${scriptFilePath%/*}/$folderName" # Change for custom directory
 
-# tun0 fetcher
-ip_address=$(ip -o -4 addr show tun0 2>/dev/null | awk '{print $4}' | cut -d '/' -f 1)
+ip_fetcher
 
 directory_exists
 directory_printing
